91f6ff5a368cc7c213428c08246752b0a4eb923b
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-10-21 14:32:30 -0400

    http request in console

--- iTunes.js
+++ iTunes.js
@@ -1,4 +1,27 @@
-angular.module('iTunes', ['ngResource'])
-  .controller('iTunesCtrl', function ($scope, $resource) {
-  $scope.greeting = 'yo'
-})
+angular.module('iTunes', [])
+  .controller('iTunesCtrl', function($scope, $http) {
+
+    if ($scope.search == undefined){
+      $scope.search = "Sherlock Holmes"
+      fetch($scope.search);
+    } else {
+      fetch($scope.search);
+    }
+    // var pendingSearch;
+    // $scope.change= function(){
+    //   if(pendingSearch){
+    //     clearTimeout(pendingSearch);
+    //   }
+    //   pendingSearch = setTimeout(fetch,1000);
+    // };
+    function fetch(name) {
+      $http({
+        method: "GET",
+        url: "http://www.omdbapi.com/?t="+name
+      }).then(function successCallback(response){
+        console.log(response)
+      }, function errorCallback(response){
+        console.log("error " + response)
+      });
+    };
+});
