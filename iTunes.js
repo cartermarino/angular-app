f1c682f6240e4c00931e253fac239d4dd3d31712
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-10-21 15:12:10 -0400

    info on page

--- iTunes.js
+++ iTunes.js
@@ -1,27 +1,27 @@
 angular.module('iTunes', [])
   .controller('iTunesCtrl', function($scope, $http) {
 
+
     if ($scope.search == undefined){
       $scope.search = "Sherlock Holmes"
       fetch($scope.search);
+      $scope.search= ''
     } else {
       fetch($scope.search);
     }
-    // var pendingSearch;
-    // $scope.change= function(){
-    //   if(pendingSearch){
-    //     clearTimeout(pendingSearch);
-    //   }
-    //   pendingSearch = setTimeout(fetch,1000);
-    // };
+
+
     function fetch(name) {
       $http({
         method: "GET",
         url: "http://www.omdbapi.com/?t="+name
       }).then(function successCallback(response){
-        console.log(response)
+        console.log(response.data)
+        $scope.title=response.data.Title
+        movieTitle = response.data.Title
       }, function errorCallback(response){
         console.log("error " + response)
       });
     };
+
 });
