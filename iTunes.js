ed4b62ca2e031c119d82123d1ce856ea844c9b52
Christopher Smith <smithopher87@gmail.com>
2015-10-21 14:44:50 -0400

    had to commit

--- iTunes.js
+++ iTunes.js
@@ -1,4 +1,12 @@
 angular.module('iTunes', ['ngResource'])
   .controller('iTunesCtrl', function ($scope, $resource) {
-  $scope.greeting = 'yo'
+
+  $http({
+    method: 'GET',
+    url: 'http://www.omdbapi.com/?s=star_wars'
+  }).then(function successCallback(response) {
+    console.log(response)
+  }, function errorCallback(response) {
+    console.log('shit blew up');
+  });
 })
