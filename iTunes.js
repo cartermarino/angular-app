7d76bfb9e85471351e027fece129c27892aa4602
Brian Deutsch <bgdeutsch@gmail.com>
2015-10-21 15:14:48 -0400

    stupid

--- iTunes.js
+++ iTunes.js
@@ -1,9 +1,9 @@
-angular.module('iTunes', ['ngResource']);
+// angular.module('iTunes', ['ngResource']);
 
-function iTunesCtrl($scope, $resource) {
-  $scope.iTunes = $resource('https://itunes.apple.com/:action',
-    {action: 'search.json', , callback: 'JSON_CALLBACK'}
-    {get: {method: 'JSONP'}})
+// function iTunesCtrl($scope, $resource) {
+//   $scope.iTunes = $resource('https://itunes.apple.com/:action',
+//     {action: 'search.json', , callback: 'JSON_CALLBACK'}
+//     {get: {method: 'JSONP'}})
 
-  $scope.iTunesResult = $scope.iTunes.get();
-}
+//   $scope.iTunesResult = $scope.iTunes.get();
+// }
