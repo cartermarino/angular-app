0a078a6059be9c82ef62f842cb21cea6b7387011
Christopher Smith <smithopher87@gmail.com>
2015-10-21 12:23:23 -0400

    corrected boilerplate

--- iTunes.js
+++ iTunes.js
@@ -1,9 +1,4 @@
-angular.module('iTunes', ['ngResource']);
-
-function iTunesCtrl($scope, $resource) {
-  $scope.iTunes = $resource('https://itunes.apple.com/:action',
-    {action: 'search.json', , callback: 'JSON_CALLBACK'}
-    {get: {method: 'JSONP'}})
-
-  $scope.iTunesResult = $scope.iTunes.get();
-}
+angular.module('iTunes', ['ngResource'])
+  .controller('iTunesCtrl', function ($scope, $resource) {
+  $scope.greeting = 'yo'
+})
